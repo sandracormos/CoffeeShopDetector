eef6f4c57f0d01cc0747deb4fa28153514e9b254
DESKTOP-OGNF42M\FairyMental <sandracormos2002@gmail.com>
2025-02-19 20:35:51 +0100

    Added functionality for Http request

---
+++ CoffeeShopDetector/CoffeeShopDetector/CsvLocalFilePathRepository.cs
@@ -0,0 +1,60 @@
+﻿namespace CoffeeShopDetector
+{
+    internal class CsvLocalFilePathRepository : Repository<CoffeeShop>
+    {
+        public override List<CoffeeShop> GetAll()
+        {
+            if (elements != null)
+            {
+                return elements;
+            }
+            throw new Exception("List is empty");
+        }
+
+        public override CoffeeShop GetAt(int index)
+        {
+            if (index < 0 || index >= elements.Count)
+                throw new IndexOutOfRangeException();
+
+            return elements[index];
+        }
+        //Initializes the application by reading CSV data from the specified file path
+        public override void Initialize(string filePath)
+        {
+            var csvData = string.Empty;
+            try
+            {
+                csvData = File.ReadAllText(filePath);
+
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"Error: The file at path '{filePath}' was not found.");
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+            LoadFromCsvString(csvData);
+        }
+        //Parses the provided CSV data and creates CoffeeShop objects from each valid line
+        // Each line is split into fields and validated to ensure it contains exactly 3 values (name, x, y)
+        protected void LoadFromCsvString(string csvData)
+        {
+            if (string.IsNullOrEmpty(csvData))
+                return;
+
+            foreach (var line in csvData.Split('\n'))
+            {
+                var fields = line.Split(',');
+                if (fields.Length == 3 && Double.TryParse(fields[1], out var x) && Double.TryParse(fields[2], out var y))
+                {
+                    elements.Add(new CoffeeShop(x, y, Name: fields[0]));
+                }
+                else
+                {
+                    throw new Exception("Invalid data format");
+                }
+            }
+        }
+    }
+}
---
+++ CoffeeShopDetector/CoffeeShopDetector/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CoffeeShopDetector
+{
+    internal static  class StringExtensions
+    {
+        //Validates if the input string is a properly formatted HTTP or HTTPS URL using a regular expression
+        public static bool IsValidHttpUrl(this string input)
+        {
+            //regex pattern for a valid HTTP/HTTPS URL
+            string pattern = @"^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(:[0-9]{1,5})?(\/[^\s]*)?$";
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(input);
+        }
+    }
+}
